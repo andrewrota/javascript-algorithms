18c2f1f857591cef554e7e214ad51b5c6c96d959
Andrew Rota <rota.andrew@gmail.com>
2013-09-07 18:46:48 -0400

    Refactor and rename

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 var assert = require('assert');
-var QuickFind = require('../src/QuickFind');
+var QuickFind = require('../src/QuickFindCollection');
 describe('Dynamic Connectivity', function(){
   describe('Quickfind', function(){
     it('should find connected items', function(){
-      var quickFind = new QuickFind(10);
-      quickFind.union(4,3);
-      quickFind.union(3,8);
-      quickFind.union(6,5);
-      quickFind.union(9,4);
-      quickFind.union(2,1);
-      assert.equal(quickFind.connected(4,3), true);
-      assert.equal(quickFind.connected(3,4), true);
-      assert.equal(quickFind.connected(0,7), false);
-      assert.equal(quickFind.connected(4,8), true);
-      assert.equal(quickFind.connected(3,9), true);
+      var network = new QuickFind(10);
+      network.union(4,3);
+      network.union(3,8);
+      network.union(6,5);
+      network.union(9,4);
+      network.union(2,1);
+      assert.equal(network.connected(4,3), true);
+      assert.equal(network.connected(3,4), true);
+      assert.equal(network.connected(0,7), false);
+      assert.equal(network.connected(4,8), true);
+      assert.equal(network.connected(3,9), true);
     });
   });
 });
