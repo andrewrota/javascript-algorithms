066ea93d7d9b7a0e5a39ab726b24d0ee9ecce531
Andrew Rota <rota.andrew@gmail.com>
2013-10-14 20:41:46 -0400

    Add method structure for Stack

--- src/Stack.js
+++ src/Stack.js
@@ -8,4 +8,55 @@
 
 module.exports = function() {
 
+  /**
+   * The array of items in the stack
+   *
+   * @private
+   * @property _items
+   * @type {Array}
+   */
+
+   var _items = [];
+
+  /**
+   * Adds an item to the stack
+   *
+   * @method push
+   * @param {Object} item Adds an item to the stack
+   */
+  var push = function(item) {};
+
+  /**
+   * Adds an item to the stack
+   *
+   * @method pop
+   * @param {Object} item Removes an item from the stack
+   */
+  var pop = function() {};
+
+  /**
+   * Checks whether the stack is empty
+   *
+   * @method isEmpty
+   * @return {Boolean} Returns true if the stack is empty
+   */
+  var isEmpty = function() {};
+
+  /**
+   * Returns the number of items in the stack
+   *
+   * @method size
+   * @return {Number} Returns number of items in the stack
+   */
+  var size = function() {};
+
+  return {
+    push: push,
+    pop: pop,
+    isEmpty: isEmpty,
+    size: size,
+    items: _items
+  };
+
+
 };
