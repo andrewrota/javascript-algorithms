4ee6c7a09b98b117cc156fe8738753a2af9419f1
Andrew Rota <rota.andrew@gmail.com>
2015-05-31 20:30:07 -0400

    Add top-down merge sort implementation

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -11,8 +11,44 @@ var util = require('../src/util');
  * @param {Array} arr The array to sort
  * @return {Array} Returns the sorted array
  */
-module.exports = function (arr) {
+module.exports = function(arr) {
   var length = arr.length;
-  
-  return arr;
+  var merge = function(a, lo, mid, hi) {
+    var aux = a.slice();
+    var i = lo;
+    var j = mid + 1;
+    for (var k = lo; k <= hi; k++) {
+      if (i > mid) {
+        a[k] = aux[j];
+        j++;
+      }
+      else if (j > hi) {
+        a[k] = aux[i];
+        i++;
+      }
+      else if (aux[j] < aux[i]) {
+        a[k] = aux[j];
+        j++;
+      }
+      else {
+        a[k] = aux[i];
+        i++;
+      }
+    }
+    return a;
+  };
+  var sort = function(a, lo, hi) {
+    if(hi <= lo) {
+      return;
+    }
+    var mid = Math.floor(lo + (hi - lo) / 2);
+    // sort lo --> mid
+    sort(a, lo, mid);
+    // sort mid --> hi
+    sort(a, mid + 1, hi);
+    // merge results
+    merge(a, lo, mid, hi);
+    return a;
+  };
+  return sort(arr, 0, arr.length -1);
 };
